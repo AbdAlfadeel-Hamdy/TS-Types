1d6b4c2cade55a4d470b35f64b3f8e8cd4ac304b
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-13 05:35:38 +0200

    type guards

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,37 @@
 "use strict";
+const e1 = {
+    name: "Max",
+    privileges: ["create-server"],
+    startDate: new Date(),
+};
+const printEmployeeInfo = (emp) => {
+    if ("privileges" in emp)
+        console.log(emp.privileges);
+    if ("startDate" in emp)
+        console.log(emp.startDate);
+};
+printEmployeeInfo({ startDate: new Date(), name: "Adham", privileges: [] });
+class Car {
+    drive() {
+        console.log("Driving...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving");
+    }
+    loadCargo(amount) {
+        console.log("Loading " + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+const useVehicle = (vehicle) => {
+    vehicle.drive();
+    if ("loadCargo" in vehicle)
+        vehicle.loadCargo(12);
+    if (vehicle instanceof Truck)
+        vehicle.loadCargo(20);
+};
+useVehicle(v2);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -21,3 +21,42 @@ const e1: ElevatedEmployee = {
 type General = string | number;
 type Numeric = number | boolean;
 type Universal = General & Numeric;
+
+// Type Guards
+
+type UnknownEmployee = Admin | Employee;
+
+const printEmployeeInfo = (emp: UnknownEmployee) => {
+  if ("privileges" in emp) console.log(emp.privileges);
+  if ("startDate" in emp) console.log(emp.startDate);
+};
+
+printEmployeeInfo({ startDate: new Date(), name: "Adham", privileges: [] });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading " + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+const useVehicle = (vehicle: Vehicle) => {
+  vehicle.drive();
+  if ("loadCargo" in vehicle) vehicle.loadCargo(12);
+  if (vehicle instanceof Truck) vehicle.loadCargo(20);
+};
+
+useVehicle(v2);
